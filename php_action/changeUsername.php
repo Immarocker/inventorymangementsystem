a773f2f9f3e5e910b8eb21c766c749f80b98a9f4
Immarocker <np034scs220107heraldcollege@edu.np>
2023-04-01 23:08:39 +0545

    Edit Featuers added

---
+++ php_action/changeUsername.php
@@ -0,0 +1,23 @@
+require_once 'core.php';
+
+if($_POST) {
+
+	$valid['success'] = array('success' => false, 'messages' => array());
+
+	$username = $_POST['username'];
+	$userId = $_POST['user_id'];
+
+	$sql = "UPDATE users SET username = '$username' WHERE user_id = $userId";
+	if($connect->query($sql) === TRUE) {
+		$valid['success'] = true;
+		$valid['messages'] = "Successfully updated";	
+	} else {
+		$valid['success'] = false;
+		$valid['messages'] = "Error while updating username";
+	}
+
+	$connect->close();
+
+	echo json_encode($valid);
+
+}
